eb6ac28ebf3a6d98c9aca8b324ad2ea8d4a33747
Tomás Coêlho <tomascoelho6@gmail.com>
2022-03-10 09:48:58 -0300

    Move result to a separete file

--- lib/pico/case.rb
+++ lib/pico/case.rb
@@ -1,64 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "case/version"
+require_relative "case/result"
 
 module Pico
   module Case
     class Error < StandardError; end
-
-    class Result
-      attr_reader :data
-
-      def self.Success(**data)
-        raise ArgumentError if data.empty?
-
-        new(:success, data)
-      end
-
-      def self.Failure(**data)
-        raise ArgumentError if data.empty?
-
-        new(:failure, data)
-      end
-
-      def initialize(type, data)
-        @type = type
-        @data = data
-      end
-
-      def success?
-        type == :success
-      end
-
-      def failure?
-        !success?
-      end
-
-      def then(&block)
-        return self if failure?
-        raise LocalJumpError if block.nil?
-
-        block_result = Result.module_exec(**data, &block)
-
-        raise ArgumentError unless block_result.is_a?(Result)
-
-        block_result
-      end
-
-      private_class_method :new
-
-      private
-
-      attr_reader :type
-
-      def respond_to_missing?(name, include_private = false)
-        @data.respond_to?(name, include_private)
-      end
-
-      def method_missing(method, *args, &block)
-        @data.send(method, *args, &block)
-      end
-    end
 
     def self.new(...)
       Result::Success(...)
